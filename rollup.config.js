65460198e19b143ba0fcef54a4e946441db6e3c1
Oscar Busk <oscar.busk@gmail.com>
2018-07-09 12:46:17 +0200

    Upgrade rollup and associated configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,23 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import nodeGlobals from 'rollup-plugin-node-globals';
 import nodeBuiltins from 'rollup-plugin-node-builtins';
 import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import pascalCase from 'pascal-case';
 
 const pkg = require('./package');
 
 export default {
-  moduleName: pascalCase(pkg.name),
-  amd: {
-    id: pkg.name,
+  input: 'es/index.js',
+  output: {
+    name: pascalCase(pkg.name),
+    amd: {
+      id: pkg.name,
+    },
+    file: 'dist/bundle.js',
+    format: 'umd',
+    exports: 'named',
+    sourceMap: true,
   },
-  entry: 'es/index.js',
-  dest: 'dist/bundle.js',
-  format: 'umd',
-  exports: 'named',
-  sourceMap: true,
   plugins: [
     sourcemaps(),
     nodeResolve(),
