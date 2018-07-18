91f3ece22ca83796dbf1ce2a2277e4b7c9a484d3
Oscar Busk <oscar.busk@gmail.com>
2018-07-18 20:02:02 +0200

    Split tests into two to show function of relativeTests

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,13 +1,8 @@
 import { HelloWorld, doSomethingAsync } from '..';
-import * as utils from '../utils';
 
 test('hello world', () => {
   const thing = new HelloWorld('neat');
   expect(thing.getMessage()).toBe('Hello, this is neat');
-});
-
-test('utils', () => {
-  expect(utils.add(1, 4)).toBe(5);
 });
 
 test('async', async () => {
